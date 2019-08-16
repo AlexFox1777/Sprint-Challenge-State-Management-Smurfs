6a0c218f5aa4a1fc077851b38a7738ec34c2a302
Sasha <mix.filipov@mail.ru>
2019-08-16 13:23:29 -0700

    mvp

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,10 @@
+export {
+    getData,
+    GET_SMURFS_START ,
+    GET_SMURFS_SUCCESS,
+    GET_SMURFS_FAILURE,
+    addSmurf,
+    ADD_SMURF_START,
+    ADD_SMURF_SUCCESS,
+    ADD_SMURF_FAILURE,
+} from './smurfAction'
